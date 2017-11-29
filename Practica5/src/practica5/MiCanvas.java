62ce9c790975ec0ba776f9a7d863cf1a22689b3e
AlmuHS <liberamenso10000@gmail.com>
2017-11-29 22:34:14 +0100

    Añadida licencia y mejorado canvas

--- Practica5/src/practica5/MiCanvas.java
+++ Practica5/src/practica5/MiCanvas.java
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017 Almudena García Jurado-Centurión
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package practica5;
 
 import java.awt.Canvas;
@@ -71,6 +83,7 @@ public class MiCanvas extends Canvas {
             }
         }
         
+        //Linea de separacion de seccion critica
         og.setColor(Color.RED);
         og.drawLine(100, 115, 300, 115);
         
@@ -93,6 +106,8 @@ public class MiCanvas extends Canvas {
         og.setColor(Color.BLUE);
         og.drawString("Escritores", 320, 90);
         
+        og.setColor(Color.RED);
+        og.drawString("Seccion critica", 300, 180);
         
         //Dibujamos la imagen
         g.drawImage(offscreen, 0, 0, null);
