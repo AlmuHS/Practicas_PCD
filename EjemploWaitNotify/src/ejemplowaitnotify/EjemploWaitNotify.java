ab597d9686d9d441cd6201258312416deae07f4f
AlmuHS <liberamenso10000@gmail.com>
2017-12-24 18:01:04 +0100

    Añadida licencia

--- EjemploWaitNotify/src/ejemplowaitnotify/EjemploWaitNotify.java
+++ EjemploWaitNotify/src/ejemplowaitnotify/EjemploWaitNotify.java
@@ -1,8 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2017 Almudena García Jurado-Centurión
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 package ejemplowaitnotify;
 
 import java.util.logging.Level;
@@ -18,7 +31,7 @@ public class EjemploWaitNotify {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-        // TODO code application logic here
+
         
         Hilo[] h = new Hilo[5];
         Compartido c = new Compartido(0);
